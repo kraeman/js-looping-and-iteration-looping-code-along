ecb3c71f94f0825073b2dedddaacbb51d11b8bb3
krae <kraewind@gmail.com>
2021-04-27 11:14:51 -0400

    commit tree f24470ed805a7b89b3097165202b40125e750a59

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names) {
+    let newArray = []
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return newArray
+}
+
+function countDown(number) {
+    while (number > -1) {
+        console.log(number--);
+    }
+}
